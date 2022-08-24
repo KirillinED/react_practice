e5820a8e2511600801475e868895ad3886fae66a
Evgeny Kirillin <jenek96@gmail.com>
2022-08-24 21:38:16 +0300

    Реализация sidebar

--- src/App.js
+++ src/App.js
@@ -3,23 +3,20 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { FiSettings } from "react-icons/fi";
 import { TooltipComponent } from "@syncfusion/ej2-react-popups";
 
-import {Navbar, Footer, Sidebar, ThemeSettings} from "./components";
-import {ECommerce, Orders, Calendar, Employees, Stacked, Pyramid, Customers, Kanban, Area, Bar, Pie, Financial, ColorPicker, ColorMapping, Editor, Line} from "./pages";
+import { Navbar, Footer, Sidebar, ThemeSettings } from "./components";
+import { ECommerce, Orders, Calendar, Employees, Stacked, Pyramid, Customers, Kanban, Area, Bar, Pie, Financial, ColorPicker, ColorMapping, Editor, Line } from "./pages";
 import './App.css';
+import { useStateContext } from "./contexts/ContextProvider";
 
 const App = () => {
-    const activeMenu = true;
-
-    useEffect(() => {
-        // activeMenu = false;
-    }, [activeMenu]);
+    const { activeMenu } = useStateContext();
 
     return (
         <div>
             <BrowserRouter>
                 <div className={"flex relative dark:bg-main-dark-bg"}>
                     <div className={"fixed right-4 bottom-4"} style={{ zIndex: '1000' }}>
-                        <TooltipComponent content={"Settings"} position={"Top"}>
+                        <TooltipComponent content={"Settings"} position={"Center"}>
                             <button type={"button"}
                                     className={"text-3xl p-3 hover:drop-shadow-xl hover:bg-light-gray text-white"}
                                     style={{
